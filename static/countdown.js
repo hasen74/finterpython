02142a874f5a06bc7a04d9255ffaacfc24c0c3b9
solano_b <solano_b@etna-alternance.net>
2023-04-20 13:27:05 +0200

    added: all studio pages

--- static/countdown.js
+++ static/countdown.js
@@ -1,29 +1,28 @@
 function countdown() {
   // Bind the timer display to the update function
-  // via the clock variable and set start at 60
+  // via the clock variable
   var clock = document.querySelector('.clock');
-  var time = 10;
 
-  function updateClock() {
-      // Call to the time flask endpoint
-      fetch('/time')
-          // Retrieve the number and display it
-          .then(response => response.json())
-          .then(data => {
-              time = data.time;
-              clock.textContent = time;
-              // At 0 trigger sound function and resets
-              if (time == 0) {
-                  checkPeople();
-                  clearInterval(interval);
-                  // countdown();
-              }
-          });
+  // Timer triggers distanciation check
+  function CountdownTimer() {
+    let countdown = 10;
+  
+    let intervalId = setInterval(() => {
+      if (countdown > 0) {
+        countdown -= 1;
+        clock.textContent = countdown;
+      } else {
+        clearInterval(intervalId);
+        intervalId = CountdownTimer();
+        checkPeople();
+      }
+    }, 1000);
+  
+    return intervalId;
   }
 
-  updateClock();
-  // Call the updateinterval every second
-  var interval = setInterval(updateClock, 1000);
+  // Launch timer
+  intervalId = CountdownTimer();
 }
 
 countdown();
